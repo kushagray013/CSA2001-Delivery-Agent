1684098ed7ec237a65e41dcfb7a3d95a21d02f22
kushagray013 <kushagray013@gmail.com>
2025-09-25 05:33:27 +0530

    Add files via upload

--- tests/test_planners.py
+++ tests/test_planners.py
@@ -1,1 +1,22 @@
+from src.grid import Grid
+from src.astar import AStarPlanner
+from src.bfs import BFSPlanner
+from src.ucs import UCSPlanner
 
+def test_astar_small():
+    g = Grid.parse_from_file("maps/map_small.txt")
+    planner = AStarPlanner(g)
+    res = planner.plan((0,0),(4,6))
+    assert res.cost < float("inf")
+
+def test_bfs_small():
+    g = Grid.parse_from_file("maps/map_small.txt")
+    planner = BFSPlanner(g)
+    res = planner.plan((0,0),(4,6))
+    assert res.cost < float("inf")
+
+def test_ucs_small():
+    g = Grid.parse_from_file("maps/map_small.txt")
+    planner = UCSPlanner(g)
+    res = planner.plan((0,0),(4,6))
+    assert res.cost < float("inf")
